fca3752a5981c91e44b40281d050722120842f24
LeeDieu <924392410@qq.com>
2021-08-13 02:03:15 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-button type="info" @click="quit">退出</el-button>
+</template>
+
+<script>
+export default {
+  methods: {
+    quit() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>>
+
